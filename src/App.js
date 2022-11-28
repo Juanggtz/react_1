c2981e3630d437e09e8d122362b960eabfc35b44
Juanggtz <juanggtz15@gmail.com>
2022-11-27 21:19:29 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const titulo = React.createElement(
+    'h1',
+    {className:'App'},
+    'LA HORA ES:',
+  );
+  //root.render(titulo);
+
+  const element = (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {titulo}
+        <h2>En México {new Date().toLocaleTimeString('es-MX')}</h2>
+        <h2>En Estados Unidos {new Date().toLocaleTimeString('es-MX',{timeZone:'America/Los_Angeles'})}</h2>
       </header>
     </div>
   );
+  root.render(element);
 }
 
 export default App;
